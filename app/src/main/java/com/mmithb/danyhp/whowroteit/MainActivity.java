61ccdc5b58597e644b45e9159a0593c5bd89ca4c
danyhp <dany.panggabean@gmail.com>
2018-04-12 13:24:26 +0700

    Initial commit

---
+++ app/src/main/java/com/mmithb/danyhp/whowroteit/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mmithb.danyhp.whowroteit;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mBookInput;
+    TextView mAuthorText;
+    TextView mTitleText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mAuthorText = findViewById(R.id.authorText);
+        mTitleText = findViewById(R.id.titleText);
+
+    }
+
+    public void searchBooks(View view) {
+
+        String queryString = mBookInput.getText().toString();
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText("Please enter a search term");
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText("Please check your network connection and try again");
+            }
+        }
+
+
+//        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+    }
+}
